b1bcfab1c0fa1c9fa1120150515f002bc348b9da
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-06-24 20:53:24 +0530

    Update ts_app_v0.2.py

--- ts_app_v0.2.py
+++ ts_app_v0.2.py
@@ -104,7 +104,7 @@ if data_source == "File Upload" :
 
     with col2: 
 
-        with st.expander("**Trend components**"):
+        with st.expander("**Trend**"):
             st.write("Add or remove components:")
             daily = st.checkbox("Daily")
             weekly= st.checkbox("Weekly")
@@ -116,3 +116,84 @@ if data_source == "File Upload" :
         with st.expander("**Seasonality**"):
             st.markdown("""The default seasonality used is additive, but the best choice depends on the specific case, therefore specific domain knowledge is required.""")
             seasonality = st.radio(label='Seasonality',options=['additive','multiplicative'])
+
+    with col4: 
+        
+        with st.expander("**Growth**"):
+            st.write('Prophet uses by default a linear growth model.')         
+
+            growth = st.radio(label='Growth model',options=['linear',"logistic"]) 
+            if growth == 'linear':
+                growth_settings= {'cap':1,'floor':0}
+                cap=1
+                floor=0
+                df['cap']=1
+                df['floor']=0
+
+            if growth == 'logistic':
+                st.info('Configure saturation')
+                cap = st.slider('Cap',min_value=0.0,max_value=1.0,step=0.05)
+                floor = st.slider('Floor',min_value=0.0,max_value=1.0,step=0.05)
+                if floor > cap:
+                    st.error('Invalid settings. Cap must be higher than floor.')
+                    growth_settings={}
+
+                if floor == cap:
+                    st.warning('Cap must be higher than floor')
+                else:
+                    growth_settings = {'cap':cap,'floor':floor}
+                    df['cap']=cap
+                    df['floor']=floor
+
+    with col5: 
+
+        with st.expander('**Holidays**'):    
+            countries = ['Country name','United States','India', 'United Kingdom', 'France','Germany']
+            with st.container():
+                years=datetime.now().year
+                selected_country = st.selectbox(label="Select country",options=countries)
+                #holidays = st.checkbox('Add country holidays to the model')
+
+                if selected_country == 'India':
+                    for date, name in sorted(holidays.IN(years=years).items()):
+                        st.write(date,name)
+                        country_code = 'IN' 
+                            
+                if selected_country == 'United Kingdom':
+                    for date, name in sorted(holidays.GB(years=years).items()):
+                        st.write(date,name)
+                        country_code = 'GB'                      
+
+                if selected_country == 'United States':                   
+                    for date, name in sorted(holidays.US(years=years).items()):
+                        st.write(date,name)
+                        country_code = 'US'
+
+                if selected_country == 'France':                    
+                    for date, name in sorted(holidays.FR(years=years).items()):
+                        st.write(date,name)
+                        country_code = 'FR'
+                            
+                if selected_country == 'Germany':                    
+                    for date, name in sorted(holidays.DE(years=years).items()):
+                        st.write(date,name)
+                        country_code = 'DE'
+
+                else:
+                    holidays = False
+                            
+                holidays = st.checkbox('Add country holidays to the model')
+
+    with col6: 
+
+        with st.expander('**Hyperparameters**'):
+            st.write('In this section it is possible to tune the scaling coefficients.')
+            
+            seasonality_scale_values= [0.01, 0.1, 1.0, 10.0]    
+            changepoint_scale_values= [0.001, 0.01, 0.1, 0.5]
+
+            st.write("The changepoint prior scale determines the flexibility of the trend, and in particular how much the trend changes at the trend changepoints.")
+            changepoint_scale= st.select_slider(label= 'Changepoint prior scale',options=changepoint_scale_values)
+            
+            st.write("The seasonality change point controls the flexibility of the seasonality.")
+            seasonality_scale= st.select_slider(label= 'Seasonality prior scale',options=seasonality_scale_values)
