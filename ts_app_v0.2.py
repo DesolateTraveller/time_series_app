a2dc3e86a5917c16fc6429a95bc82ad58deca43f
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-06-25 19:17:27 +0530

    Update ts_app_v0.2.py

--- ts_app_v0.2.py
+++ ts_app_v0.2.py
@@ -91,7 +91,7 @@ if data_source == "File Upload" :
 #-----------------------------------------------------------------------------------------------------
 
     #st.subheader("Parameters", divider='blue')
-    st.markdown("**In this section you can modify the algorithm settings.**")
+    #st.markdown("**In this section you can modify the algorithm settings.**")
 
     col1, col2, col3, col4, col5, col6 = st.columns(6)
 
